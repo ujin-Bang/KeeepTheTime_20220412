becd8e8f2269f33b365f568142c1ace180ed0995
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-12 11:25:05 +0900

    Initial commit

---
+++ app/src/main/java/com/time/keeepthetime_20220412/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.time.keeepthetime_20220412
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
