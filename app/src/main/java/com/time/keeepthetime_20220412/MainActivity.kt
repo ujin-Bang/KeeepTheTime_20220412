4045495bc235a434d6497685ad015138433ec2cb
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-12 11:36:38 +0900

    메인화면 > BaseActivity 상속

--- app/src/main/java/com/time/keeepthetime_20220412/MainActivity.kt
+++ app/src/main/java/com/time/keeepthetime_20220412/MainActivity.kt
@@ -5,12 +5,22 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.time.keeepthetime_20220412.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
