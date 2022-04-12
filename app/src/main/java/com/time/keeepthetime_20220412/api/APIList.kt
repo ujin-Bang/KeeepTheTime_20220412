4ee10695d302a2f6200b1030749df4d0de02dbd4
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-12 20:00:37 +0900

    인터페이스에 로그인 기능을 위한 함수 명세 작성

--- app/src/main/java/com/time/keeepthetime_20220412/api/APIList.kt
+++ app/src/main/java/com/time/keeepthetime_20220412/api/APIList.kt
@@ -1,4 +1,17 @@
 package com.time.keeepthetime_20220412.api
 
-class APIList {
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.POST
+
+interface APIList {
+
+//    BASE_URL에 대당하는 서버에서 어떤 기능들을 사용할건지 명시
+
+    @POST("/user")
+   fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password" ) pw: String
+    ):Call<JSONObject>
 }
